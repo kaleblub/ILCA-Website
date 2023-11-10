4c3df0420b3b8458415cb5dd2e8217b172f72411
Kaleb <kaleblub@gmail.com>
2023-11-10 18:33:31 +0200

    Added vercel database info and configurations

--- ILCA_Website/settings.py
+++ ILCA_Website/settings.py
@@ -10,8 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+import dj_database_url
+from decouple import config
+
+PRISMA_URL = config('PRISMA_URL', default='')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,10 +85,9 @@ WSGI_APPLICATION = 'ILCA_Website.wsgi.app'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('PRISMA_URL')
+    )
 }
 
 
