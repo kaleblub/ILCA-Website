cc7b01b71b0693a693ecca970460778ba7599353
Kaleb <kaleblub@gmail.com>
2023-11-10 18:45:02 +0200

    Added 'login_required'

--- registration/views.py
+++ registration/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def registration(request):
 	return render(request, 'registration/register.html')
+
+@login_required
+def login(request):
+	return render(request, 'registration/login.html')
