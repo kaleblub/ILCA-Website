cea9252786d80f6abdeb12ea9e24a2f334eb38df
Kaleb <kaleblub@gmail.com>
2023-12-23 21:56:42 -0500

    Changed the requirements file to production

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,7 @@
 
 echo "Installing pip Packages"
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install -r requirements/production.txt
 
 echo "Collecting Static Files"
 # mkdir staticfiles
