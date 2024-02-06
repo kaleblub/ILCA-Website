bdfadbe752974144df431d61f50fd28cf61badbf
Kaleb <kaleblub@gmail.com>
2024-02-05 21:12:00 -0500

    Added a crappy rough draft of a contact form on the contact page.

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
 
 def contact(request):
-	return render(request, 'contact/contact.html')
+	if request.method == 'POST':
+		form = ContactForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('contact_success')
+	else:
+		form = ContactForm()
+
+	return render(request, 'contact/contact.html', {'form': form})
