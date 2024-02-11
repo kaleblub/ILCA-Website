ed5d3e8ee6008d1a4fe114580652805a4a716d81
Kaleb <kaleblub@gmail.com>
2024-02-11 16:22:08 -0500

    Fix language dropdown menu and prepared some of the information on the about page to be recieved from the db.

--- about/views.py
+++ about/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
 
 def about(request):
-	return render(request, 'about/about.html')
+	courses_and_info = {
+		'Advanced Courses':'Dive deep into language intricacies and advanced communication skills with our specialized courses.', 
+		'Intermediate Courses':'Build on your existing language foundation and enhance your proficiency with our intermediate courses.', 
+		'Beginner Courses':'Lay a solid foundation for your language journey with our beginner-friendly courses designed to make learning engaging and effective.', 
+		'Group Lessons':'Collaborate and learn in a supportive group setting, fostering a sense of community and shared progress.', 
+		'Private Lessons':'Receive personalized attention and guidance from our expert instructors with one-on-one private lessons.', 
+		'Adult Courses':'Tailored for adult learners, our courses cater to the specific needs and goals of individuals at all proficiency levels.', 
+		'Classes for Children':'Nurture a love for languages in young minds with our engaging and age-appropriate language classes for children.', 
+		'Academic Support for Each Level':'Our commitment to your success goes beyond the classroom. Benefit from academic support tailored to your specific proficiency level.'}
+	teammembers = [
+		('1', 'Instructor Name', 'Designation'), 
+		('2', 'Instructor Name', 'Designation'), 
+		('3', 'Instructor Name', 'Designation'), 
+		('4', 'Instructor Name', 'Designation'),
+	]
+	return render(request, 'about/about.html', {'courses_and_info': courses_and_info, 'teammembers': teammembers})
