d746ef235096700a48c8e8b71fa794401613f16b
Kaleb <kaleblub@gmail.com>
2024-02-25 12:36:24 -0500

    Limiting requirements to make sure every one is used and not wasting space.

--- build_files.sh
+++ build_files.sh
@@ -5,7 +5,7 @@ python3 -m venv venv
 echo "--Activating virtual environment--"
 source venv/bin/activate
 echo "--Installing requirements--"
-pip install -r requirements/production.txt
+pip install -r requirements.txt # /production.txt
 echo "--Upgrading pip and installing psycopg2-binary--"
 /vercel/path0/venv/python3 -m pip install --upgrade pip
 pip install psycopg2-binary
