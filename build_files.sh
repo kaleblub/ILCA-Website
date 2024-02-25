ecc8b141b7bd9aa18ced92ad14d3a189e0c91512
Kaleb <kaleblub@gmail.com>
2024-02-25 11:03:59 -0500

    Trying to fix an error with psycopg2-binary on vercel

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 echo "Installing pip Packages"
-python3 -m pip install --upgrade pip
+/vercel/path0/venv/python3 -m pip install --upgrade pip
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements/production.txt
+pip install psycopg2-binary
 
 echo "Collecting Static Files"
 python3 manage.py collectstatic --noinput
