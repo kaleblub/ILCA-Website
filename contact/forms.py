d10fe26d11614be219b0beb0cb65a1ac74663ac2
Kaleb <kaleblub@gmail.com>
2024-02-25 10:36:23 -0500

    Added a basic form to the contact page.

--- contact/forms.py
+++ contact/forms.py
@@ -4,4 +4,4 @@ from .models import ContactMessage
 class ContactForm(forms.ModelForm):
     class Meta:
         model = ContactMessage
-        fields = ['name', 'email', 'message']
+        fields = ['name', 'email', 'subject', 'message']
--- contact/models.py
+++ contact/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class ContactMessage(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
+    subject = models.CharField(max_length=100)
     message = models.TextField()
 
     def __str__(self):
