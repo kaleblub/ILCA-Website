b3a9257b38cdc0a750543d0c0a2778bb5ef6ea4d
Kaleb <kaleblub@gmail.com>
2024-02-25 11:11:59 -0500

    Added debugging print statements

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-echo "Installing pip Packages"
-/vercel/path0/venv/python3 -m pip install --upgrade pip
+echo "--Creating virtual environment--"
 python3 -m venv venv
+echo "--Activating virtual environment--"
 source venv/bin/activate
+echo "--Installing requirements--"
 pip install -r requirements/production.txt
+echo "--Upgrading pip and installing psycopg2-binary--"
+/vercel/path0/venv/python3 -m pip install --upgrade pip
 pip install psycopg2-binary
 
-echo "Collecting Static Files"
+echo "--Collecting Static Files--"
 python3 manage.py collectstatic --noinput
 
 # Set PYTHONPATH to the project root directory
